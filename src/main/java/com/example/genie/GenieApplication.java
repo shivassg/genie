0bb3739528f13bf1ab1bef49e12d6598297a670b
shivassg <shivaletters@gmail.com>
2021-12-02 15:30:33 -0500

    Added Rest API Endpoints

--- src/main/java/com/example/genie/GenieApplication.java
+++ src/main/java/com/example/genie/GenieApplication.java
@@ -1,10 +1,20 @@
 package com.example.genie;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
 public class GenieApplication {
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GenieApplication.class, args);
